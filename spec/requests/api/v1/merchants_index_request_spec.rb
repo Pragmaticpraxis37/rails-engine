bb100bb5bdf3d6b9b03b556997e81bd71bf0c23f
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-03 11:02:27 -0600

    Add happy path tests that all pass

--- spec/requests/api/v1/merchants_index_request_spec.rb
+++ spec/requests/api/v1/merchants_index_request_spec.rb
@@ -16,50 +16,46 @@ describe 'Merchants Index API' do
       expect(merchants[:data]).to be_an(Array)
     end
 
-    xit 'sends a list of merchants with attributes' do
+    it 'sends a list of merchants with attributes' do
       get api_v1_merchants_path
 
       expect(response).to be_successful
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      # Need to dig in farther into attributes
-
-      merchants.each do |merchant|
+      merchants[:data].each do |merchant|
         expect(merchant).to have_key(:id)
-        expect(merchant).to be_an(Integer)
-
+        expect(merchant[:id]).to be_an(String)
         expect(merchant).to have_key(:type)
-        expect(merchant).to be_a(String)
-
+        expect(merchant[:type]).to be_a(String)
         expect(merchant).to have_key(:attributes)
-        expect(merchant).to be_a(String)
+        expect(merchant[:attributes]).to be_a(Hash)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
       end
     end
 
-    xit 'sends a list of merchants 20 and one page by default if no query params are specified' do
+    it 'sends a list of merchants 20 and one page by default if no query params are specified' do
       get api_v1_merchants_path
 
       expect(response).to be_successful
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      # Might need to open array more
       expect(merchants[:data].count).to eq(20)
     end
 
-    xit 'sends a list of 50 merchants if requested in per_page query param' do
+    it 'sends a list of 50 merchants if requested in per_page query param' do
       get api_v1_merchants_path, params: {per_page: 50, page: 1}
 
       expect(response).to be_successful
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      # Might need to open array more
       expect(merchants[:data].count).to eq(50)
     end
 
-    xit 'sends a list of merchants 51-100 if per_page query param is 50 and
+    it 'sends a list of merchants 51-100 if per_page query param is 50 and
         page query param is 2' do
       get api_v1_merchants_path, params: {per_page: 50, page: 2}
 
@@ -67,15 +63,17 @@ describe 'Merchants Index API' do
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      count = 51
+      id = @merchants[50].id
 
-      merchants.each do |merchant|
-        expect(merchant[:data][:id]).to eq(count)
-        count += 1
+      expect(merchants[:data].count).to eq(50)
+
+      merchants[:data].each do |merchant|
+        expect(merchant[:id].to_i).to eq(id)
+        id += 1
       end
     end
 
-    xit 'sends a list of all merchants if requested in per_page query param
+    it 'sends a list of all merchants if requested in per_page query param
         is as large as the number of merchants' do
       get api_v1_merchants_path, params: {per_page: 100, page: 1}
 
@@ -83,11 +81,10 @@ describe 'Merchants Index API' do
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      # Might need to open array more
       expect(merchants[:data].count).to eq(100)
     end
 
-    xit 'sends a list of all merchants if requested in per_page query param
+    it 'sends a list of all merchants if requested in per_page query param
         is larger than the number of merchants' do
       get api_v1_merchants_path, params: {per_page: 101, page: 1}
 
@@ -95,8 +92,24 @@ describe 'Merchants Index API' do
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      # Might need to open array more
       expect(merchants[:data].count).to eq(100)
     end
+
+    it 'sends an data array if there is no return data for the query' do
+      get api_v1_merchants_path, params: {per_page: 75, page: 3}
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data]).to be_an(Array)
+      expect(merchants[:data].count).to eq(0)
+    end
+  end
+
+  describe 'sad path' do
+
+
+
   end
 end
