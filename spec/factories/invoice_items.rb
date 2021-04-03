63c66cf2e646880c3ffc80b4df94de1321a4b252
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-03 07:47:12 -0600

    Update factory param

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice_item do
     # item { nil }
     # invoice { nil }
-    quantity { Faker::Number.number(digits: 1) }
+    quantity { Faker::Number.within(range: 1..100) }
     unit_price { Faker::Number.decimal(l_digits: 2) }
     item
     invoice
