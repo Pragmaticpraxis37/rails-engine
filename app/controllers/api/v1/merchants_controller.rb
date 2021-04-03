2fc3f9b33bb52ad7e741d95f607d949cf239d8aa
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-03 11:00:12 -0600

    Finish index action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,11 +3,9 @@ module Api
     class MerchantsController < ApplicationController
 
         def index
-          if params[:per_page].nil? == true && params[:page].nil? == true
-            all_merchants = MerchantsFacade.all_merchants(params[:per_page].to_i, params[:page].to_i)
-          elsif params[:per_page].nil? == true
-          end
-
+          merchants = MerchantsFacade.merchants_index(params)
+          render json: MerchantSerializer.new(merchants)
+          # require "pry"; binding.pry
         end
 
 
