534582efb2d3788fd2d4c8d0f9982acf32deb703
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-04 10:17:20 -0600

    Add show action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,10 +2,13 @@ module Api
   module V1
     class ItemsController < ApplicationController
       def index
-        # require "pry"; binding.pry
         items = ItemsFacade.items_index(params)
-        # require "pry"; binding.pry
         render json: ItemSerializer.new(items)
+      end
+
+      def show
+        item = ItemsFacade.items_show(params[:id].to_i)
+        render json: ItemSerializer.new(item)
       end
     end
   end
