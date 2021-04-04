be6e26e889c0380346f40b71b26b2a6de97b12f4
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-04 12:26:42 -0600

    Add happy path tests that all pass

---
+++ spec/requests/api/v1/items_create_request_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+describe 'Items Create API' do
+  before :each do
+    @merchant = create(:merchant)
+  end
+
+  it 'can create a new item and save it to the Item database' do
+    item_params = ({
+      name: 'Wobbler',
+      description: 'Wobbles',
+      unit_price: 12.50,
+      merchant_id: @merchant.id
+      })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
+
+    expect(response).to be_successful
+
+
+    created_item = Item.last
+
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it 'can create a new item and save it to the Item database' do
+    item_params = ({
+      name: 'Wobbler',
+      description: 'Wobbles',
+      unit_price: 12.50,
+      merchant_id: @merchant.id
+      })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    # require "pry"; binding.pry
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_an(String)
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data][:type]).to be_a(String)
+    expect(item[:data][:type]).to eq('item')
+    expect(item[:data]).to have_key(:attributes)
+    expect(item[:data][:attributes]).to be_a(Hash)
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+  end
+end
