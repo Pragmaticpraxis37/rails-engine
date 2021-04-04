056ffd3fd5987a3749dfdc5f72175b7f20e3a836
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-04 09:17:56 -0600

    Add #self.obtain_items and #self.offset_count

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.obtain_items(per_page, page)
+    limit(per_page).offset(self.offset_count(per_page, page))
+  end
+
+  def self.offset_count(per_page, page)
+    if page.to_i <= 0 then page = 1 end
+    unless page.to_i == 1
+      (page.to_i - 1) * per_page
+    end
+  end
 end
