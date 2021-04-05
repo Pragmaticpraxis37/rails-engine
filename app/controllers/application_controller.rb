1d1ec549fd3919a0af4c18449ea185c8d7241625
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-05 08:19:27 -0600

    Add ActiveRecord::RecordInvalid but it has been commented out because it causes ActiveRecord::RecordNotFound to fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # resuce_from ActiveRecord::RecordInvalid, with: :render_not_created_response
 
   def render_not_found_response(exception)
     render json: { error: exception.message }, status: :not_found
   end
+
+  # def render_not_created_response
+  #   render json: { error: exception.message }, status: :not_created
+  # end
 end
