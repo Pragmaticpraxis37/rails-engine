811a3a7735d7e572d29d054ebfef0acedfd013c4
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-05 13:47:44 -0600

    Add in final test set up

--- spec/requests/api/v1/items_destory_request_spec.rb
+++ spec/requests/api/v1/items_destory_request_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 describe 'Merchants Index API' do
   before :each do
     @merchant_1 = create(:merchant)
-    @merchant_2 = create(:merchant)
     @item_1 = create(:item, merchant: @merchant_1)
-    @item_2 = create(:item, merchant: @merchant_1)
-    @item_3 = create(:item, merchant: @merchant_2)
-    @item_4 = create(:item, merchant: @merchant_2)
     @invoice_1 = create(:invoice, merchant: @merchant_1)
-    @invoice_item_1 = create(:item, merchant: @merchant_1)
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1)
+
+    @merchant_2 = create(:merchant)
+    @item_2 = create(:item, merchant: @merchant_2)
+    @item_3 = create(:item, merchant: @merchant_2)
+    @invoice_2 = create(:invoice, merchant: @merchant_2)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2)
+    @invoice_item_1 = create(:invoice_item, item: @item_3, invoice: @invoice_2)
   end
 
   describe 'happy path' do
