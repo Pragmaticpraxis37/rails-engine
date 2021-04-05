6bedfd2416ecc4ee33c1b85b9a0f96af07ceda66
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-05 07:40:37 -0600

    Add first sad path test

--- spec/requests/api/v1/items_create_request_spec.rb
+++ spec/requests/api/v1/items_create_request_spec.rb
@@ -5,61 +5,102 @@ describe 'Items Create API' do
     @merchant = create(:merchant)
   end
 
-  it 'can create a new item and save it to the Item database' do
-    item_params = ({
-      name: 'Wobbler',
-      description: 'Wobbles',
-      unit_price: 12.50,
-      merchant_id: @merchant.id
-      })
+  describe 'happy path' do
+    it 'can create a new item and save it to the Item database' do
+      item_params = ({
+        name: 'Wobbler',
+        description: 'Wobbles',
+        unit_price: 12.50,
+        merchant_id: @merchant.id
+        })
 
-    headers = {"CONTENT_TYPE" => "application/json"}
+      headers = {"CONTENT_TYPE" => "application/json"}
 
-    post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
+      post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
 
-    created_item = Item.last
+      created_item = Item.last
 
-    expect(created_item.name).to eq(item_params[:name])
-    expect(created_item.description).to eq(item_params[:description])
-    expect(created_item.unit_price).to eq(item_params[:unit_price])
-    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    end
+
+    it 'can create a new item and save it to the Item database' do
+      item_params = ({
+        name: 'Wobbler',
+        description: 'Wobbles',
+        unit_price: 12.50,
+        merchant_id: @merchant.id
+        })
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_an(String)
+      expect(item[:data]).to have_key(:type)
+      expect(item[:data][:type]).to be_a(String)
+      expect(item[:data][:type]).to eq('item')
+      expect(item[:data]).to have_key(:attributes)
+      expect(item[:data][:attributes]).to be_a(Hash)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+      expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+    end
   end
 
-  it 'can create a new item and save it to the Item database' do
-    item_params = ({
-      name: 'Wobbler',
-      description: 'Wobbles',
-      unit_price: 12.50,
-      merchant_id: @merchant.id
-      })
+  describe 'sad path' do
+    it  'only accepts a JSON body with the following fields: name, description,
+        unit_price, and merchant_id' do
 
-    headers = {"CONTENT_TYPE" => "application/json"}
+      item_params = ({
+        name: 'Wobbler',
+        description: 'Wobbles',
+        unit_price: 12.50,
+        merchant_id: @merchant.id,
+        review: 'This is a great product'
+        })
 
-    post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
+      headers = {"CONTENT_TYPE" => "application/json"}
 
-    expect(response).to be_successful
+      post api_v1_items_path, headers: headers, params: JSON.generate(item: item_params)
 
-    item = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
 
-    # require "pry"; binding.pry
+      item = JSON.parse(response.body, symbolize_names: true)
 
-    expect(item[:data]).to have_key(:id)
-    expect(item[:data][:id]).to be_an(String)
-    expect(item[:data]).to have_key(:type)
-    expect(item[:data][:type]).to be_a(String)
-    expect(item[:data][:type]).to eq('item')
-    expect(item[:data]).to have_key(:attributes)
-    expect(item[:data][:attributes]).to be_a(Hash)
-    expect(item[:data][:attributes]).to have_key(:name)
-    expect(item[:data][:attributes][:name]).to be_a(String)
-    expect(item[:data][:attributes]).to have_key(:description)
-    expect(item[:data][:attributes][:description]).to be_a(String)
-    expect(item[:data][:attributes]).to have_key(:unit_price)
-    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
-    expect(item[:data][:attributes]).to have_key(:merchant_id)
-    expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+      require "pry"; binding.pry
+
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_an(String)
+      expect(item[:data]).to have_key(:type)
+      expect(item[:data][:type]).to be_a(String)
+      expect(item[:data][:type]).to eq('item')
+      expect(item[:data]).to have_key(:attributes)
+      expect(item[:data][:attributes]).to be_a(Hash)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+      expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:data]).to_not have_key(:review)
+    end
   end
 end
