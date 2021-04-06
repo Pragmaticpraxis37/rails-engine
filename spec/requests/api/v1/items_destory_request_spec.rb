7ff97fecfa87f597b5094e466bd3bdc703ca0ff6
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-06 06:11:28 -0600

    Complete test

--- spec/requests/api/v1/items_destory_request_spec.rb
+++ spec/requests/api/v1/items_destory_request_spec.rb
@@ -2,29 +2,43 @@ require 'rails_helper'
 
 describe 'Item Destroy API' do
   before :each do
-    @merchant_1 = create(:merchant)
-    @item_1 = create(:item, merchant: @merchant_1)
-    @invoice_1 = create(:invoice, merchant: @merchant_1)
-    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1)
-
     @merchant_2 = create(:merchant)
     @item_2 = create(:item, merchant: @merchant_2)
+    @invoice_1 = create(:invoice, merchant: @merchant_2)
+    @invoice_item_1 = create(:invoice_item, item: @item_2, invoice: @invoice_1)
+
+    # @merchant_2 = create(:merchant)
+    # @item_2 = create(:item, merchant: @merchant_2)
+
+
+
+
     @item_3 = create(:item, merchant: @merchant_2)
     @invoice_2 = create(:invoice, merchant: @merchant_2)
     @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2)
-    @invoice_item_1 = create(:invoice_item, item: @item_3, invoice: @invoice_2)
+    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_2)
+    #
+    # @item_4 = create(:item, merchant: @merchant_2)
+    # @invoice_3 = create(:invoice, merchant: @merchant_2)
+    # @invoice_item_4 = create(:invoice_item, item: @item_2, invoice: @invoice_3)
+    # @invoice_item_5 = create(:invoice_item, item: @item_4, invoice: @invoice_3)
   end
 
   describe 'happy path' do
     it "removes a single item from the Item table" do
 
-      delete api_v1_item_path(@item_1.id)
+      # require "pry"; binding.pry
 
-      expect(response).to be_successful
+      delete api_v1_item_path(@item_2.id)
 
-      item = JSON.parse(response.body, symbolize_names: true)
+      deleted = Invoice.where(id: @invoice_1.id).first
+      not_deleted = Invoice.find(@invoice_2.id)
 
-      expect(item[:data]).to be_a(Hash)
+      # require "pry"; binding.pry
+
+      expect(deleted).to be_nil
+      # (ActiveRecord::RecordNotFound)
+      expect(not_deleted).to_not be_nil
     end
   end
 end
